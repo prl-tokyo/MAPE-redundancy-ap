9aaf62ee31dc1890c86d851ab91548c9e1cd6b73
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-10 17:25:11 +0900

    Pretty print

--- src/main/java/jp/ac/nii/prl/mape/redundancy/repository/InstanceRepository.java
+++ src/main/java/jp/ac/nii/prl/mape/redundancy/repository/InstanceRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import jp.ac.nii.prl.mape.redundancy.model.Instance;
 
 public interface InstanceRepository extends JpaRepository<Instance, Long> {
-
+	
 }
