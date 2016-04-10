d3847fcf5948c465795cb7f83a77d0a56eacfa57
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-10 17:08:09 +0900

    Redundancy view model

---
+++ src/main/java/jp/ac/nii/prl/mape/redundancy/model/RedundancyView.java
@@ -0,0 +1,40 @@
+package jp.ac.nii.prl.mape.redundancy.model;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class RedundancyView {
+
+	@GeneratedValue
+	@Id
+	@JsonIgnore
+	private Long id;
+	
+	@NotEmpty
+	private Collection<Instance> instances;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Collection<Instance> getInstances() {
+		return instances;
+	}
+
+	public void setInstances(Collection<Instance> instances) {
+		this.instances = instances;
+	}
+	
+}
