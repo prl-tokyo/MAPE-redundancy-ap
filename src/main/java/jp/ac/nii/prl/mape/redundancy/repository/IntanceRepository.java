3160f3828b2343d9862bc3c7e1b30ef82d4dea14
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-10 17:26:34 +0900

    Remove moved file

--- src/main/java/jp/ac/nii/prl/mape/redundancy/repository/IntanceRepository.java
+++
@@ -1,9 +0,0 @@
-package jp.ac.nii.prl.mape.redundancy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import jp.ac.nii.prl.mape.redundancy.model.Instance;
-
-public interface IntanceRepository extends JpaRepository<Instance, Long> {
-
-}
