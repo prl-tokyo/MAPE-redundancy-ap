0d954609fca2176d06d9c16dcdc36fb4b0f40d8b
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-10 16:07:50 +0900

    Generated Spring Boot project

---
+++ src/main/java/jp/ac/nii/prl/mape/redundancy/MapeRedundancyApApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl.mape.redundancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeRedundancyApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeRedundancyApApplication.class, args);
+	}
+}
