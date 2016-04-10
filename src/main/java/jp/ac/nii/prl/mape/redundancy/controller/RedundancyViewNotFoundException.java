9ffb135a6d14a3523db0b0e20cbebffe7cebef64
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-10 22:10:22 +0900

    Exception to use when RedundancyView can't be found

---
+++ src/main/java/jp/ac/nii/prl/mape/redundancy/controller/RedundancyViewNotFoundException.java
@@ -0,0 +1,13 @@
+package jp.ac.nii.prl.mape.redundancy.controller;
+
+import org.springframework.web.client.RestClientException;
+
+public class RedundancyViewNotFoundException extends RestClientException {
+
+	private static final long serialVersionUID = -1271540170754527250L;
+
+	public RedundancyViewNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
